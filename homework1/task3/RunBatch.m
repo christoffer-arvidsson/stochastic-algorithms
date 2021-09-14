3e4846454448560a748d5683ec217bf123169858
Christoffer Arvidsson <christoffer@arvidson.nu>
2021-09-14 14:19:05 +0200

    Add ; and make runBatch run multiple simulations

--- homework1/task3/RunBatch.m
+++ homework1/task3/RunBatch.m
@@ -1,4 +1,4 @@
-r%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Parameter specifications
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -17,30 +17,27 @@ crossoverProbability = 0.8;        % Do NOT change
 % Batch runs
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Define more runs here (pMut < 0.02) ...
-
-mutationProbability = 0.02;
-sprintf('Mutation rate = %0.5f', mutationProbability)
-maximumFitnessList002 = zeros(numberOfRuns,1);
-for i = 1:numberOfRuns 
- [maximumFitness, bestVariableValues]  = RunFunctionOptimization(populationSize, numberOfGenes, numberOfVariables, maximumVariableValue, tournamentSize, ...
-                                       tournamentProbability, crossoverProbability, mutationProbability, numberOfGenerations);
- sprintf('Run: %d, Score: %0.10f', i, maximumFitness)
-  maximumFitnessList002(i,1) = maximumFitness;
+numParameters = 10;
+mutationProbabilityRange = linspace(0.02, 1.0, numParameters);
+maximumFitnessList = zeros(numParameters, numberOfRuns);
+for iSearch = 1:numParameters
+  mutationProbability = mutationProbabilityRange(iSearch);
+  sprintf('Mutation rate = %0.5f', mutationProbability);
+  for i = 1:numberOfRuns
+    [maximumFitness, bestVariableValues]  = RunFunctionOptimization(populationSize, numberOfGenes, numberOfVariables, maximumVariableValue, tournamentSize, ...
+                                                                    tournamentProbability, crossoverProbability, mutationProbability, numberOfGenerations);
+    sprintf('Run: %d, Score: %0.10f', i, maximumFitness)
+    maximumFitnessList(iSearch, i) = maximumFitness;
+  end
 end
-
-
-% ... and here (pMut > 0.02)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Summary of results
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Add more results summaries here (pMut < 0.02) ...
-
-average002 = mean(maximumFitnessList002);
-median002 = median(maximumFitnessList002);
-std002 = sqrt(var(maximumFitnessList002));
-sprintf('PMut = 0.02: Median: %0.10f, Average: %0.10f, STD: %0.10f', median002, average002, std002)
-
-% ... and here (pMut > 0.02)
+for iSearch = 1:numParameters
+  average = mean(maximumFitnessList(i));
+  median = median(maximumFitnessList(i));
+  std = sqrt(var(maximumFitnessList(i)));
+  sprintf('PMut = 0.02: Median: %0.10f, Average: %0.10f, STD: %0.10f', median, average, std)
+end
